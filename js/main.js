f404ea2fc9edfc029a9a145c4d11439a543a7501
Jasper Soleymanzadeh <jaspersoleymanzadeh@gmail.com>
2022-02-10 11:01:20 -0600

    basic functionality and some bonuses

--- js/main.js
+++ js/main.js
@@ -1,25 +1,73 @@
 const url = "https://rough-harvest-liver.glitch.me/movies"
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        let html = ''
-        for(let i = 0; i < data.length; i++){
-                $('body').append(`${data[i].title}`)
-        }
-        $("#loading").attr('style', 'display: none')
-
-
-    })
-    .catch(err => console.log(err))
-$("addMovieButton").submit(function(e){
-        e.preventDefault();
-        $(this).submit();
-        // fetch(url, {
-        //         method: 'POST'
-        // })
-})
+let drawMovies = () => fetch(url)
+	.then(response => response.json())
+	.then(data => {
+		let html = ''
+		for (let i = 0; i < data.length; i++) {
+			$('.container').append(`<div>
+				${data[i].title}
+				<button id="${data[i].id}delete" data-id=${data[i].id}>Delete This Movie</button>
+				</div>`);
+			$(`#${data[i].id}delete`).click(() => {
+				$(`#${data[i].id}delete`).attr("disabled", true);
+				fetch(url + "/" + data[i].id, {
+					method: "DELETE", headers: {'Content-Type': 'application/json'}
+				})
+					.then(response => {
+						$(".container").html("");
+						drawMovies();
+					})
+					.catch(error => console.log(error));
+			});
+			moviesArray.push(data[i]);
+		}
+		$("#loading").attr('style', 'display: none');
+	})
+	.catch(err => console.log(err));
+$("#addMovieButton").click(function () {
+	$("#addMovieButton").attr("disabled", true);
+	fetch(url, {
+		method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({
+			title: $("#title").val(),
+			director: "",
+			year: "",
+			genre: "",
+			actors: "",
+			plot: "",
+			rating: $("input[name='rating']:checked").val(),
+			poster: ""
+		})
+	})
+		.then(response => {
+			$(".container").html("");
+			drawMovies();
+			$("#addMovieButton").attr("disabled", false);
+		})
+		.catch(error => console.log(error));
+});
+$("#editMovieButton").click(() => {
+	$("#editMovieButton").attr("disabled", true);
+	fetch(url + "/" + $("#movieId").val(), {
+		method: "PUT", headers: {'Content-Type': 'application/json'}, body: JSON.stringify({
+			title: $("#title").val(),
+			director: "",
+			year: "",
+			genre: "",
+			actors: "",
+			plot: "",
+			rating: $("input[name='rating']:checked").val(),
+			poster: ""
+		})
+	})
+		.then(response => {
+			$(".container").html("");
+			drawMovies();
+			$("#editMovieButton").attr("disabled", false);
+		})
+		.catch(error => console.log(error));
+});
+let moviesArray = [];
+drawMovies();
 let movieBody = {
-        title: $("#title").val(),
-        rating: $("#")
+	title: $("#title").val(), rating: $("#")
 }
